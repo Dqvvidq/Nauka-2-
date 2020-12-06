ef1166fc1305f44cbe927bb3817da80d85e02fd2
Dqvvidq <74021650+Dqvvidq@users.noreply.github.com>
2020-12-06 22:29:58 +0100

    Add files via upload

---
+++ Instrukcje.py
@@ -0,0 +1,41 @@
+#Instrukcje
+
+print("Witaj swiecie")
+
+2 + 2
+
+for i in range(10):
+    print("Witaj swiecie")
+
+print("Czesc")
+print("kon")
+print("Jou")
+
+x = 5
+if x < 10:
+    print("x jest mniejsze od 10")
+if x >= 10:
+    print("x jest większe lub równe 10")
+
+y = 12
+if y <= 10:
+    print("y jest mniejsze lub rowne 10")
+if y > 10:
+    if y <= 25:
+        print("y jest większe od 10, lecz mniejsze lob rowne 25")
+if y > 25:
+    print("y jest wieksze od 25")
+
+#y == 12
+m = 6
+print(y/m)
+print(y%m)
+
+age = 23
+if age == 23:
+    print("age wynosi 23")
+elif age == 24:
+    print("age wynosi 24")
+elif age == 25:
+    print("age wynosi 25")
+
