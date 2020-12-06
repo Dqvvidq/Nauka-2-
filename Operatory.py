47d96ce57a74239b091e61fc1e6f174472e96ece
Dqvvidq <74021650+Dqvvidq@users.noreply.github.com>
2020-12-06 21:42:06 +0100

    Add files via upload

---
+++ Operatory.py
@@ -0,0 +1,22 @@
+#Operatory Arytmetyczne
+
+print(2**2) #operator potęgowania
+print(15%2) #modulo/reszta z dzielenia
+print(14//4) #dzielenie całkowite/ zaokrąglenie w dol
+print(20/5) #dzielenie
+print(2*2) #mnożenie
+print(10-7) #odejmowanie
+print(9+3) #dodawanie
+
+#Operatory porównana
+print(10>2) #większe
+print(3<10) #większe
+print(10>=10) #większe lub rowne
+print(10<=10) #mniesze lub rowne
+print(2==2) #rowne
+print(10!=5) #rozne
+
+#Operatory logiczne
+print(2==2 and 2>1) #Koniukcja logiczna
+print(3<1 or 2<4) #Alternatywa logiczna
+print(not True) #Negacja
