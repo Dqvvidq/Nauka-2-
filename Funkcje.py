0c337385de42b6e3921851fc60bd822fe956d873
Dqvvidq <74021650+Dqvvidq@users.noreply.github.com>
2020-12-08 21:32:24 +0100

    Add files via upload

---
+++ Funkcje.py
@@ -0,0 +1,27 @@
+#Funkcje
+
+def f(x):
+    return x * 2
+
+result = f(5)
+print(result)
+
+if result == 4:
+    print("Wynik wynosi 4")
+else:
+    print("Wynik nie jest rowny 4")
+
+def g():
+    return 1 + 1
+
+rez = g()
+print(rez)
+
+
+def h(x, y, z):
+    return x + y + z
+
+rezu = h(1, 2, 3)
+print(rezu)
+
+
